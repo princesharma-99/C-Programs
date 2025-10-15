db7648d19e111f1636a4e24fad93fd31ef5cc707
Prince Sharma <bs135585@gmail.com>
2025-10-15 20:40:49 +0530

    value copy behavior

---
+++ call_by_value.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void swap(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+    printf("Inside function (after swap): x = %d, y = %d\n", x, y);
+}
+
+int main() {
+    int a = 10, b = 20;
+    printf("Before function call: a = %d, b = %d\n", a, b);
+    swap(a, b);
+    printf("After function call: a = %d, b = %d\n", a, b);
+    return 0;
+}
