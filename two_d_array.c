b89e1a7135992efc8808b4cadc65868b9987399b
Prince Sharma <bs135585@gmail.com>
2025-10-16 20:28:15 +0530

    2D matrix input/output

---
+++ two_d_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("\nMatrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
