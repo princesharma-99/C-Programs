7b5673ea0c1f5574ff559335eb57ebce78928e7c
Prince Sharma <bs135585@gmail.com>
2025-10-16 21:25:34 +0530

    Bubble & Selection sorting

---
+++ sorting_algorithms.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n, i, choice;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nChoose sorting method:\n1. Bubble Sort\n2. Selection Sort\nChoice: ");
+    scanf("%d", &choice);
+
+    if (choice == 1)
+        bubbleSort(arr, n);
+    else
+        selectionSort(arr, n);
+
+    printf("\nSorted Array: ");
+    display(arr, n);
+
+    return 0;
+}
